e400c1979955f0470bacc0271831336a65e285a1
Alex Koval <al3xander.koval@gmail.com>
2024-05-15 14:50:45 +0700

    improve setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,20 @@ project_name="mvp"
 dockerhub_username="hetsketch"
 prod_ip="1.1.1.1"
 
+main () {
+  rename_project
+  rename_database_names
+  rename_docker_compose
+  rename_cable
+  prepare_for_deploy
+  exec $SHELL
+}
+
 rename_project () {
   echo "Renaming project"
   cd . || return
   new_dir=${PWD%/*}/$project_name
   mv -- "$PWD" "$new_dir" && cd -- "$new_dir"
-  exec $SHELL
 }
 
 remove_git_origin () {
@@ -18,25 +26,24 @@ remove_git_origin () {
 
 rename_database_names () {
   echo "Renaming database.yml"
-  sed -i "s/rapid_rails/$project_name/g" config/database.yml
+  sed -i '' "s/rapid_rails/$project_name/g" config/database.yml
 }
 
 rename_docker_compose () {
-  sed -i "s/rapid_rails/$project_name/g" docker-compose.yml
+  echo "Renaming docker-compose.yml"
+  sed -i '' "s/rapid_rails/$project_name/g" docker-compose.yml
 }
 
 rename_cable () {
-  sed -i "s/rapid_rails/$project_name/g" config/cable.yml
+  echo "Renaming cable.yml"
+  sed -i '' "s/rapid_rails/$project_name/g" config/cable.yml
 }
 
 prepare_for_deploy () {
-  sed -i "s/<prod_ip>/$prod_ip/g" config/deploy.yml
-  sed -i "s/hetsketch/$dockerhub_username/g" config/deploy.yml
-  sed -i "s/rapid_rails/$project_name/g" config/deploy.yml
+  echo "Renaming deploy.yml"
+  sed -i '' "s/<prod_ip>/$prod_ip/g" config/deploy.yml
+  sed -i '' "s/hetsketch/$dockerhub_username/g" config/deploy.yml
+  sed -i '' "s/rapid_rails/$project_name/g" config/deploy.yml
 }
 
-rename_project
-rename_database_names
-rename_docker_compose
-rename_cable
-prepare_for_deploy
+main
