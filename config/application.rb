c3d69277bd5a633a925fa425d185cf6abc6acd95
Alex Koval <al3xander.koval@gmail.com>
2024-12-15 14:02:49 +0100

    update to rails 8

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "boot"
 require "rails/all"
 
@@ -7,9 +5,14 @@ Bundler.require(*Rails.groups)
 
 module LightningRails
   class Application < Rails::Application
-    config.load_defaults 7.0
+    config.load_defaults 8.0
 
     config.active_job.queue_adapter = :sidekiq
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks generators])
 
     config.generators do |g|
       g.test_framework :rspec, fixtures: false, view_specs: false, helper_specs: false,
